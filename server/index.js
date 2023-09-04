d30c9e89d0f52c2709bf72151ee710a86bb767d8
Srushti Shete <shetesrushti11@gmail.com>
2023-09-04 11:25:06 +0530

    Backend configuration and setup completed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -16,4 +20,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
