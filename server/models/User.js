38ee2140f6597697733ee6a6ddc7e2f1f6e89e57
Srushti Shete <shetesrushti11@gmail.com>
2023-09-04 23:01:01 +0530

    Created MongoDB Schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,36 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      min: 2,
-      max: 100,
-    },
-    email: {
-      type: String,
-      required: true,
-      max: 50,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 5,
-    },
-    city: String,
-    state: String,
-    country: String,
-    occupation: String,
-    phoneNumber: String,
-    transactions: Array,
-    role: {
-      type: String,
-      enum: ["user", "admin", "superadmin"],
-      default: "admin",
-    },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 100,
   },
-  { timestamps: true }
-);
-
+  email:{
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+  },
+  password:{
+    type: String,
+    required: true,
+    min: 5,
+  },
+  city: String,
+  state: String,
+  country: String,
+  occupation: String,
+  phoneNumber: String,
+  transactions: Array,
+  role:{
+    type: String,
+    enum: ["user", "admin","superadmin"],
+    default: "admin"
+  },
+},
+  {timestamps: true}
+)
 const User = mongoose.model("User", UserSchema);
 export default User;
