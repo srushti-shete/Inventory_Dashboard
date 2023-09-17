230e0a6fee7d18f3302657a9e94b27d5b6f56abb
Srushti Shete <shetesrushti11@gmail.com>
2023-09-17 22:30:05 +0530

    Defined getter function for geography

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,9 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
+
 
 export const getProducts = async (req, res) => {
     try {
@@ -72,3 +75,27 @@ export const getProducts = async (req, res) => {
     }
   };
   
+  export const getGeography = async (req, res) => {
+    try {
+      const users = await User.find();
+  
+      const mappedLocations = users.reduce((acc, { country }) => {
+        const countryISO3 = getCountryIso3(country);
+        if (!acc[countryISO3]) {
+          acc[countryISO3] = 0;
+        }
+        acc[countryISO3]++;
+        return acc;
+      }, {});
+  
+      const formattedLocations = Object.entries(mappedLocations).map(
+        ([country, count]) => {
+          return { id: country, value: count };
+        }
+      );
+  
+      res.status(200).json(formattedLocations);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  };
