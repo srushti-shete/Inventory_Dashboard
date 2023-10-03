ec99f87ec7a7e5c811f96433e2b2ef77e1ec1721
Srushti Shete <shetesrushti11@gmail.com>
2023-10-03 19:59:52 +0530

    Connected backend with frontend using getter function

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -12,6 +12,7 @@ export const api = createApi({
     "Sales",
     "Admins",
     "Performance",
+    "Dashboard",
   ],
     endpoints: (build) => ({
       getUser: build.query({
@@ -50,8 +51,12 @@ export const api = createApi({
         query: (id) => `management/performance/${id}`,
         providesTags: ["Performance"],
       }),
+      getDashboard: build.query({
+        query: () => "general/dashboard",
+        providesTags: ["Dashboard"],
+      }),
 
   })
 })
 
-export const {useGetUserQuery, useGetProductsQuery,  useGetCustomersQuery, useGetTransactionsQuery,  useGetGeographyQuery,  useGetSalesQuery, useGetAdminsQuery, useGetUserPerformanceQuery, } =api;
+export const {useGetUserQuery, useGetProductsQuery,  useGetCustomersQuery, useGetTransactionsQuery,  useGetGeographyQuery,  useGetSalesQuery, useGetAdminsQuery, useGetUserPerformanceQuery, useGetDashboardQuery, } =api;
