4db9a1456caab8ec6d3a043ba7de8b21058f883a
Srushti Shete <shetesrushti11@gmail.com>
2023-10-03 19:47:19 +0530

    Injected data for scenes

--- server/index.js
+++ server/index.js
@@ -12,11 +12,12 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser, dataProduct, dataProductStat,dataTransaction, dataOverallStat } from "./data/index.js";
+import { dataUser, dataProduct, dataProductStat,dataTransaction, dataOverallStat, dataAffiliateStat, } from "./data/index.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -51,6 +52,7 @@ mongoose
     // User.insertMany(dataUser);
     // Transaction.insertMany(dataTransaction);
     //  OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
 
   })
   .catch((error) => console.log(`${error} did not connect`));
