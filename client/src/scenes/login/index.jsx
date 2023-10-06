de5ed7a62e5a89a4e5dd3dd2886c8a97137b292d
Amaan Jambura <amaanjambura786@gmail.com>
2023-10-07 00:56:15 +0530

    Completed Designing of Login and Signup pages

--- client/src/scenes/login/index.jsx
+++ client/src/scenes/login/index.jsx
@@ -1,103 +1,28 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 import Avatar from "@mui/material/Avatar";
-import API_URL from '../../config/config';
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Alert from '@mui/material/Alert';
-import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import {
   Box,
   Button,
   Typography,
   useTheme,
-  Link,
   Grid,
   TextField,
 } from "@mui/material";
 
-// import { themeSettings } from "../theme";
-
 const Login = () => {
-  const navigate = useNavigate();
-
-  // States for registration
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  // States for checking the errors
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  // Handling the email change
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const user = {
-    email,
-    password,
-  };
-
-  console.log(user);
-  // Handling the form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (email === "" || password === "") {
-      setError(true);
-      setErrorMessage("Please fill all the fields");
-      return;
-    } else {
-      console.log("DONE")
-      const response = await fetch(`${API_URL}/general/login`, {
-        method: "POST",
-        withCredentials: true,
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      const data = await response.json();
-      console.log(data);
-      if (data.error) {
-        setError(true);
-        setErrorMessage(data.error);
-      } else {
-        setError(false);
-        console.log("loggedIn");
-        localStorage.setItem("DashBoardUserLoggedIn", true);
-        localStorage.setItem("DashBoardUser", JSON.stringify(data));
-        navigate("/dashboard");
-      }
-    }
-  }
-
-  const DisplayErrorMessage = () => {
-    return (
-       error ?  <Alert severity="error" sx={{ background: "white",
-        color:"black",
-        fontWeight: "bold"
-        }}>
-          {errorMessage}
-        </Alert> : ""
-    );
-  };
-
-//   const DisplayCredentials = () => {
-//     return (
-//         <Alert severity="success">FOR TESTING PURPOSES USE
-//         Email: swelbeck12@ycombinator.com, Password: password
-//         </Alert> 
-//     );
-//   };
+  
   const theme = useTheme();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
 
   return (
     <Box
@@ -111,7 +36,6 @@ const Login = () => {
         gap: "3rem"
       }}
     >
-      <DisplayErrorMessage />
       <Box
         pt="2rem"
         sx={{
@@ -126,7 +50,7 @@ const Login = () => {
         <Header title="WELCOME" subtitle="Sign In to your dashboard" />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 3,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
@@ -136,13 +60,12 @@ const Login = () => {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Login
+            Log In          
           </Typography>
           <Box
             m="1rem"
             component="form"
             noValidate
-            //onSubmit={handleSubmit}
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
@@ -154,40 +77,40 @@ const Login = () => {
                       autoComplete: "off",
                     },
                   }}
-                  onChange={handleEmail}
                   required
                   fullWidth
                   id="email"
                   label="Email Address"
                   name="email"
+                  onChange={(e) => setEmail(e.target.value)} 
+                  value={email}
                   autoFocus
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                  onChange={handlePassword}
                   required
                   fullWidth
                   name="password"
                   label="Password"
                   type="password"
                   id="password"
+                  onChange={(e) => setPassword(e.target.value)} 
+                  value={password} 
                 />
               </Grid>
             </Grid>
             <Button
-              onClick={handleSubmit}
-              type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2, p: "0.5rem 0", background: theme.palette.primary[400],
                 "&:hover": { backgroundColor: theme.palette.primary[300]} 
                   }}
+              onClick={handleSubmit}
             >
               Login
             </Button>
           </Box>
-          <DisplayCredentials />
         </Box>
       </Box>
     </Box>
---
+++ client/src/scenes/signup/index.jsx
@@ -0,0 +1,247 @@
+import React from "react";
+import { useState } from "react";
+import Header from "../../components/Header";
+import Avatar from "@mui/material/Avatar";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  Grid,
+  TextField,
+} from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const Signup = () => {
+  
+  const theme = useTheme();
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [country, setCountry] = useState('')
+  const [occupation, setOccupation] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [role, setRole] = useState('')
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(name, email, password, city, state, country, occupation, phoneNumber, role)
+  }
+
+  return (
+    <Box
+      m=" 4rem 20rem"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100%",
+        gap: "3rem"
+      }}
+    >
+      <Box
+        pt="2rem"
+        sx={{
+          backgroundColor: theme.palette.background.alt,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: "0.2rem",
+        }}
+      >
+        <Header title="WELCOME" subtitle="Create a new account" />
+        <Box
+          sx={{
+            marginTop: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "theme.palette.secondary"}}>
+            <AccountCircleOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up        
+          </Typography>
+          <Box
+            m="1rem"
+            component="form"
+            noValidate
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+            {/* name, email, password, city, state, country, occupation, phoneNumber, role */}
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  name="name"
+                  onChange={(e) => setName(e.target.value)} 
+                  value={name}
+                  autoFocus
+                />
+              </Grid>
+             {/*email, password, city, state, country, occupation, phoneNumber, role */}
+              <Grid item xs={6}>
+                <TextField
+                  inputProps={{
+                    autoComplete: "new-email",
+                    form: {
+                      autoComplete: "off",
+                    },
+                  }}
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)} 
+                  value={email}
+                  autoFocus
+                />
+              </Grid>
+            {/* city, state, country, occupation, phoneNumber, role */}
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)} 
+                  value={password} 
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phoneNumber"
+                  label="Phone Number"
+                  name="phoneNumber"
+                  onChange={(e) => setPhoneNumber(e.target.value)} 
+                  value={phoneNumber}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="role"
+                  label="Role"
+                  name="role"
+                  onChange={(e) => setRole(e.target.value)} 
+                  value={role}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="occupation"
+                  label="Occupation"
+                  name="occupation"
+                  onChange={(e) => setOccupation(e.target.value)} 
+                  value={occupation}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <InputLabel id="demo-simple-select-helper-label">City</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="City"
+                        fullWidth
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"Mumbai"}>Mumbai</MenuItem>
+                    <MenuItem value={"Thane"}>Thane</MenuItem>
+                    <MenuItem value={"Nagpur"}>Nagpur</MenuItem>
+                    </Select>
+              </Grid>
+            {/* country, occupation, phoneNumber, role */}
+            <Grid item xs={3}>
+                <InputLabel id="demo-simple-select-helper-label">State</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="State"
+                        fullWidth
+                        value={state}
+                        onChange={(e) => setState(e.target.value)}
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"Maharashtra"}>Maharashtra</MenuItem>
+                    <MenuItem value={"Gujarat"}>Gujarat</MenuItem>
+                    <MenuItem value={"Rajasthan"}>Rajasthan</MenuItem>
+                    <MenuItem value={"Jammu & Kashmir"}>Jammu & Kashmir</MenuItem>
+                    <MenuItem value={"Haryana"}>Haryana</MenuItem>
+                    <MenuItem value={"West Bengal"}>West Bengal</MenuItem>
+                    </Select>
+              </Grid>
+            {/* occupation, phoneNumber, role */}
+            <Grid item xs={3}>
+                <InputLabel id="demo-simple-select-helper-label">Country</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="Country"
+                        fullWidth
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={"India"}>India</MenuItem>
+                    <MenuItem value={"America"}>America</MenuItem>
+                    <MenuItem value={"France"}>France</MenuItem>
+                    <MenuItem value={"Greenland"}>Greenland</MenuItem>
+                    <MenuItem value={"Ireland"}>Ireland</MenuItem>
+                    <MenuItem value={"Ratnagiri"}>Ratnagiri</MenuItem>
+                </Select>
+            </Grid>
+              <Grid item xs={3}>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt:"1.8rem", p: "0.5rem 0", background: theme.palette.primary[400],
+                        "&:hover": { backgroundColor: theme.palette.primary[300]} 
+                        }}
+                    onClick={handleSubmit}
+                    >
+                    Signup
+                </Button>
+              </Grid>
+            </Grid>
+            
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Signup;
